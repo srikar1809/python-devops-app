28d0f98bcb5e6017557108211b013ad33b9a3fe9
Srikar1809 <srikar.ponna@gmail.com>
2026-09-04 18:07:39 +0530

    Add application logs

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+import logging
 
 app = Flask(__name__)
 
+logging.basicConfig(level=logging.INFO)
+
 @app.route("/")
 def home():
-    return "Hello from Azure DevOps!"
+    app.logger.info("Home endpoint was accessed")
+    return "Hello from Python DevOps App!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5002)
+    app.run(host="0.0.0.0", port=5001)
