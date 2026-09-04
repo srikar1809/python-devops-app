76c0dae874658cc34b1a4802b442a848fb8c8b49
Srikar1809 <srikar.ponna@gmail.com>
2026-09-04 18:20:45 +0530

    Fix return statement

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ logging.basicConfig(level=logging.INFO)
 @app.route("/")
 def home():
     app.logger.info("Home endpoint was accessed")
-    return "Hello from Python DevOps App!"
+    return "Hello from Azure DevOps!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
